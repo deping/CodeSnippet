ff605c385114b619257fe6287b04423d649f4af7
deping <cdp97531@sina.com>
2020-03-27 17:51:06 +0800

    Vue ContextMenu usage: <simple-context-menu :options="menuItems" ref="contextMenu" @option-clicked="onMenuItem" />     contextMenu(       event: MouseEvent,       data: DirNode,       node: TreeNode<any, DirNode>     ) {       this.menuItems = [{ name: "创建子目录", key: "create" }];       let menu = this.$refs.contextMenu; // as SimpleContextMenu;       (menu as any).showMenu(event, node);     },
        onMenuItem(node: TreeNode<any, DirNode>, menuItem: MenuItem) {
          console.log(menuItem.name + node.label);
        },

---
+++ SimpleContextMenu.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <ul ref="list" class="vue-simple-context-menu" v-click-outside="onClickOutside">
+      <li
+        v-for="(option, index) in options"
+        :key="index"
+        @click="optionClicked(option)"
+        class="vue-simple-context-menu__item"
+        :class="option.class"
+      >{{option.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import vClickOutside from "v-click-outside";
+Vue.use(vClickOutside);
+export default Vue.extend({
+  name: "VueSimpleContextMenu",
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      data: null,
+      menuWidth: null,
+      menuHeight: null
+    };
+  },
+  methods: {
+    showMenu(event, data) {
+      this.data = data;
+      var menu = this.$refs.list;
+      if (!menu) {
+        return;
+      }
+      if (!this.menuWidth || !this.menuHeight) {
+        menu.style.visibility = "hidden";
+        menu.style.display = "block";
+        this.menuWidth = menu.offsetWidth;
+        this.menuHeight = menu.offsetHeight;
+        menu.removeAttribute("style");
+      }
+      if (this.menuWidth + event.pageX >= window.innerWidth) {
+        menu.style.left = event.pageX - this.menuWidth + 2 + "px";
+      } else {
+        menu.style.left = event.pageX - 2 + "px";
+      }
+      if (this.menuHeight + event.pageY >= window.innerHeight) {
+        menu.style.top = event.pageY - this.menuHeight + 2 + "px";
+      } else {
+        menu.style.top = event.pageY - 2 + "px";
+      }
+      menu.classList.add("vue-simple-context-menu--active");
+    },
+    hideContextMenu() {
+      let element = this.$refs.list;
+      if (element) {
+        element.classList.remove("vue-simple-context-menu--active");
+      }
+    },
+    onClickOutside() {
+      this.hideContextMenu();
+    },
+    optionClicked(option) {
+      this.hideContextMenu();
+      this.$emit("option-clicked", this.data, option);
+    },
+    onEscKeyRelease(event) {
+      if (event.keyCode === 27) {
+        this.hideContextMenu();
+      }
+    }
+  },
+  mounted() {
+    document.body.addEventListener("keyup", this.onEscKeyRelease);
+  },
+  beforeDestroy() {
+    document.removeEventListener("keyup", this.onEscKeyRelease);
+  }
+});
+</script>
+
+<style lang="scss">
+$light-grey: #ecf0f1;
+$grey: darken($light-grey, 15%);
+$blue: #007aff;
+$white: #fff;
+$black: #333;
+.vue-simple-context-menu {
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  display: none;
+  list-style: none;
+  position: absolute;
+  z-index: 1000000;
+  background-color: $light-grey;
+  border-bottom-width: 0px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  box-shadow: 0 3px 6px 0 rgba($black, 0.2);
+  border-radius: 4px;
+  &--active {
+    display: block;
+  }
+  &__item {
+    display: flex;
+    color: $black;
+    cursor: pointer;
+    padding: 5px 15px;
+    align-items: center;
+    &:hover {
+      background-color: $blue;
+      color: $white;
+    }
+  }
+  // Have to use the element so we can make use of `first-of-type` and
+  // `last-of-type`
+  li {
+    white-space: nowrap;
+    &:first-of-type {
+      margin-top: 4px;
+    }
+    &:last-of-type {
+      margin-bottom: 4px;
+    }
+  }
+}
+</style>
